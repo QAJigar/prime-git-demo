30178aa08d2c33df3b08a1dba0b1904125e08d1a
QAJigar <166711044+QAJigar@users.noreply.github.com>
2024-07-20 15:40:46 +0100

    feature branch created

---
+++ src/main/java/com/gorest/PrintMyName.java
@@ -0,0 +1,14 @@
+package com.gorest;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrintMyName
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Jigar Patel" );
+    }
+
+}
